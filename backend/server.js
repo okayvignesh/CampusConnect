c41ff23cef7daac5f1a7e5129f6f547357b1900b
Vignesh <vignesh@kandradigital.com>
2023-03-29 23:36:32 +0530

    added userdata fetch

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const db = require('./db')
 const path = require('path');
 const bodyParser = require('body-parser');
-const { register, login } = require('./userController');
+const { register, login, userdata } = require('./userController');
 require('dotenv').config();
 
 
@@ -12,16 +12,33 @@ require('dotenv').config();
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-
 app.use(cors());
 app.use(express.static('dist'));
+
+
+
+
+app.get('/userdata/:param', (req, res) => {
+    const param = req.params.param;
+    const query = `SELECT * FROM users WHERE username='${param}'`;
+    db.query(query, (err, results) => {
+        if (err) {
+            return res.status(500).json({ error: err.message });
+        }
+        res.json(results);
+    });
+});
+
+
+
+app.post('/register', register)
+app.post('/login', login)
+
+
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
 });
 
-app.post('/register', register)
-app.post('/login', login)
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
